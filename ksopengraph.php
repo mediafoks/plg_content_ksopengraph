206ff058c3bbc1732e1ea570c85de06efe81b79b
mediafoks <mediafoks@gmail.com>
2024-02-07 12:47:08 +0300

    release test

--- ksopengraph.php
+++ ksopengraph.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * @version    1.0.4
+ * @version    1.0.5
  * @package    ksopengraph (plugin)
  * @author     Sergey Kuznetsov - mediafoks@google.com
  * @copyright  Copyright (c) 2024 Sergey Kuznetsov
